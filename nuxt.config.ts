3f3c81dfc57ef5f8684465d67b316a62381e9e53
Nicolas Walcker <walcker_@outlook.com>
2022-11-27 13:08:30 -0300

    chore(general): adds sass files, icons, fonts and remove tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  modules: ['@vueuse/nuxt'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: ``,
+          additionalData: `
+          @import "@/assets/scss/_variables.scss";
+          @import "@/assets/scss/_global.scss";
+          @import "@/assets/scss/_colors.scss";
+          @import "@/assets/scss/_reset.scss";
+          @import "@/assets/scss/_breakpoints.scss";
+          `,
         },
       },
+    },
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://api.fontshare.com/v2/css?f[]=switzer@600,800,900,100,700,400,200,300,500&display=swap',
+        },
+      ],
     },
   },
 });
