9e7ac57304bd7f8282217e62a82b77158c28ab5a
Nicolas Walcker <walcker_@outlook.com>
2022-11-29 02:03:25 -0300

    feat(blog and projects): create blog and projects page (in build)

---
+++ components/InBuild.vue
@@ -0,0 +1,39 @@
+<!--
+ Copyright 2022 nicolaswalcker
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<template>
+  <article class="content">
+    <h1 class="title">Em construção</h1>
+    <p><span>🚧</span><span>⚠️</span></p>
+  </article>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  font-size: 64px;
+  color: var(--color-secondary);
+}
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section"><InBuild /></section>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Projetos - Em breve 🚧',
+  meta: [
+    {
+      name: 'description',
+      content: 'Projects',
+    },
+  ],
+});
+</script>
+
+<style scoped></style>
