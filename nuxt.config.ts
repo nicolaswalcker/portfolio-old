e3f7ec60b51814ffcf4ce55ce7e1601ffbb2d6bf
Nicolas Walcker <walcker_@outlook.com>
2022-11-29 19:43:56 -0300

    perf(font): add local font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
           @import "@/assets/scss/_colors.scss";
           @import "@/assets/scss/_reset.scss";
           @import "@/assets/scss/_transitions.scss";
+          @import "@/assets/fonts/switzer/_switzer.scss";
           `,
         },
       },
@@ -23,11 +24,6 @@ export default defineNuxtConfig({
         lang: 'pt-BR',
       },
       link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://api.fontshare.com/v2/css?f[]=switzer@600,800,900,100,700,400,200,300,500&display=swap',
-          prefetch: 'true',
-        },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,500,0,0',
