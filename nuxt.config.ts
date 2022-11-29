b153c36ef5b4ce0e99d8a03bb56aa82cc34d2ec4
Nicolas Walcker <walcker_@outlook.com>
2022-11-29 02:07:40 -0300

    chore(general): adds new icon fonts from google, page transitions and some sass files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,12 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData: `
+          @import "@/assets/scss/_breakpoints.scss";
           @import "@/assets/scss/_variables.scss";
           @import "@/assets/scss/_global.scss";
           @import "@/assets/scss/_colors.scss";
           @import "@/assets/scss/_reset.scss";
-          @import "@/assets/scss/_breakpoints.scss";
+          @import "@/assets/scss/_transitions.scss";
           `,
         },
       },
@@ -18,12 +19,21 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'pt-BR',
+      },
       link: [
         {
           rel: 'stylesheet',
           href: 'https://api.fontshare.com/v2/css?f[]=switzer@600,800,900,100,700,400,200,300,500&display=swap',
+          prefetch: 'true',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,500,0,0',
         },
       ],
     },
+    pageTransition: { name: 'fade', mode: 'out-in' },
   },
 });
