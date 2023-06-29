5dc17e5db303f9412c6233737b256ff68a87ace5
Nicolas <walcker_@outlook.com>
2023-06-28 23:58:26 -0300

    fix(blog): remove items and adds inbuild component again

---
+++ pages/blog.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <InBuild />
+  </section>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Blog - Em breve 🚧',
+  meta: [
+    {
+      name: 'description',
+      content: 'Blog',
+    },
+  ],
+});
+</script>
+
+<style scoped lang="scss">
+pre {
+  max-width: 45%;
+}
+
+.section {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+</style>
